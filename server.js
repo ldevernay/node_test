0b943231755e70dda162af823a7fb7862379b2a6
ldevernay <boblehamster@yahoo.fr>
2017-03-01 11:23:14 +0100

    init handlebars

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
-const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
-
-app.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))  
